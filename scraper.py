35df731d3ef0b6c7aa2c4c9118b05fe99314daf7
kgrumke <86072108+kgrumke@users.noreply.github.com>
2021-07-19 16:14:58 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -131,38 +131,6 @@ df
 
 df.to_csv('MOSenate_BillsFiled.csv')
 
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
-message = Mail(
-    from_email=os.environ.get('FROM_EMAIL'),
-    to_emails=os.environ.get('TO_EMAIL'),
-    subject='Scraped content',
-    html_content="It's attached as an... attachment.")
-
-# https://www.twilio.com/blog/sending-email-attachments-with-twilio-sendgrid-python
-with open('MOSenate_BillsFiled.csv', 'rb') as f:
-    data = f.read()
-    f.close()
-encoded_file = base64.b64encode(data).decode()
-
-attachedFile = Attachment(
-    FileContent(encoded_file),
-    FileName('MOSenate_BillsFiled.csv'),
-    FileType('text/csv'),
-    Disposition('attachment')
-)
-message.attachment = attachedFile
-
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
 
 
 # In[241]:
